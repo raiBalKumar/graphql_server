79c0891fc41fea1a1792513234845d733b95721a
Bala-office <preitam_rai@yahoo.com>
2018-11-14 18:29:41 +0800

    type definitions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 import express from "express";
+import cors from "cors";
 import { ApolloServer, gql } from "apollo-server-express";
 
 const app = express();
 
+app.use(cors());
+
 const schema = gql`
   type Query {
     me: User
+    user(id: ID!): User
+    users: [User!]
   }
 
   type User {
+    id: ID!
     username: String!
   }
 `;
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch"
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids"
+  }
+};
+const me = users[1];
 const resolvers = {
   Query: {
     me: () => {
-      return {
-        username: "Bal Kumar Rai"
-      };
+      return me;
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    users: () => {
+      return Object.values(users);
     }
   }
 };
